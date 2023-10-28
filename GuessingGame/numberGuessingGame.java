0fed10ccdb94bc366ab13123fce8e3fecd047308
DarrenCT <99516347+DarrenCT@users.noreply.github.com>
2023-10-28 14:46:13 -0400

    Added number guessing game logic.

--- GuessingGame/numberGuessingGame.java
+++ GuessingGame/numberGuessingGame.java
@@ -1,5 +1,21 @@
 package GuessingGame;
 
 public class numberGuessingGame {
-
+	public static void main(String[] args) {
+		
+	}
+	
+	public static String determineGuess(int response, int solution, int count) {
+		if(response <= 0 || response > 100) {
+			return "Your guess must be between 0 and 100.";
+		} else if (response == solution) {
+			return "Correct!" + "\n" + "Total Guesses: " + count; 
+		} else if (response > solution) {
+			return "Your guess is too high, try again." + "\n" + "Try Number: " + count;
+		} else if (response < solution) {
+			return "Your guess is too low, try again." + "\n" + "Try Number: " + count;
+		} else {
+			return "";
+		}
+	}
 }
